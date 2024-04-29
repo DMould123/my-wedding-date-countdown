efec7d40a87a82bf108cea0156c49b3cc8ae2c64
DMould123 <95115610+DMould123@users.noreply.github.com>
2024-04-29 13:21:16 +0200

    Update the elements innerText with the new calculated values and set a function to updated the countdown. Added notes to the file to explain methods

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-const getDays = document.getElementById('days')
-const getHours = document.getElementById('hours')
-const getMinutes = document.getElementById('minutes')
-const getSeconds = document.getElementById('seconds')
+// Select the elements representing the countdown units
+const getDays = document.getElementById('wedding-days')
+const getHours = document.getElementById('wedding-hours')
+const getMinutes = document.getElementById('wedding-minutes')
+const getSeconds = document.getElementById('wedding-seconds')
 
+// Get the wedding date and time in milliseconds
 const weddingDate = new Date('Sept 14, 2024, 14:55:00').getTime()
 
+// Call the function to start updating the countdown
 updateWeddingCountdown()
 
+// Implement function to update the wedding countdown
 function updateWeddingCountdown() {
+  // Get the current time
   const current = new Date().getTime()
+
+  // Calculate the remaining time until the wedding
   const remainingTime = weddingDate - current
 
+  // Define constants for milliseconds in seconds, minutes, hours, and days
   const seconds = 1000
   const minutes = seconds * 60
   const hours = minutes * 60
   const days = hours * 24
 
+  // Calculate the remaining days, hours, minutes, and seconds
   const remainingDays = Math.floor(remainingTime / days)
-  console.log(remainingDays)
+  const remainingHours = Math.floor((remainingTime % days) / hours)
+  const remainingMinutes = Math.floor((remainingTime % hours) / minutes)
+  const remainingSeconds = Math.floor((remainingTime % minutes) / seconds)
+
+  // Update the elements with the new calculated values
+  getDays.innerText = remainingDays
+  getHours.innerText = remainingHours
+  getMinutes.innerText = remainingMinutes
+  getSeconds.innerText = remainingSeconds
+
+  // Schedule the updateWeddingCountdown function to be called again after 1 second
+  setTimeout(updateWeddingCountdown, 1000)
 }
